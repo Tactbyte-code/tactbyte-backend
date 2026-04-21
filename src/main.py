32aa86989503f276b99cb1aad0572b4a78da27f2
Tactbyte-code <interntactbyte@gmail.com>
2026-04-21 17:22:16 +0530

    update CORS allowed origins to use a configurable setting

--- src/main.py
+++ src/main.py
@@ -22,12 +22,7 @@ authX.handle_errors(app)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://admin-ppai.vercel.app",
-        "http://localhost:3001",
-        "http://localhost:3000",
-        "https://demo-server-rtl3.vercel.app",
-    ],  
+    allow_origins=settings.allowed_origins,  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
